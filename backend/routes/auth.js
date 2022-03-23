9eaba8ec5b78d5a5d612ea9ca4c9b5a3d56ae8bb
Farhad <farhadmolla618@gmail.com>
2022-03-23 22:29:33 +0530

    video-46 user validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send("<auth.js: hello>");
-})
+router.post('/',
+    [body('name').isLength({ min: 3 }),
+    body('email', '<email err>').isEmail(),
+    body('password').isLength({ min: 3 })],
+    (req, res) => {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        }).then(user => res.json(user))
+        .catch(err => {
+            res.json({error: err});
+        });
+    },
+);
 
 module.exports = router
