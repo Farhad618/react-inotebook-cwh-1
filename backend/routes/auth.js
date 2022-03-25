3a8b3220fcf7f2f73b3d3747811029c055a97848
Farhad <farhadmolla618@gmail.com>
2022-03-25 20:41:34 +0530

    video-50 login authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,6 +7,7 @@ const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = "qiuwrhdjkfgi";
 
+//user creat and data validation
 router.post(
     '/creatuser',
     [
@@ -22,10 +23,10 @@ router.post(
         }
 
         // check if the email already used
-        let isEmailUsed = await User.findOne({email: req.body.email});
+        let isEmailUsed = await User.findOne({ email: req.body.email });
         // console.log(`<auth.js: ${isEmailUsed}>`);
 
-        if(isEmailUsed){
+        if (isEmailUsed) {
             return res.status(400).json({ errors: `Use different email.` });
         }
 
@@ -40,19 +41,62 @@ router.post(
         })
             .then(user => {
                 const data = {
-                    user:{
+                    user: {
                         id: user.id
                     }
                 }
                 const authToken = jwt.sign(data, JWT_SECRET);
                 // console.log(authToken)
                 // res.json(user)
-                res.json({authToken})
+                res.json({ authToken })
             })
             .catch(err => {
                 res.json({ error: err });
             });
     },
 );
+
+//login authintication 
+router.post(
+    '/login',
+    [
+        body('email', '<email err>').isEmail(),
+        body('password', '<pappword empty>').isLength({ min: 1 })
+    ],
+    async (req, res) => {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        // check if the email already used
+        let isEmailExist = await User.findOne({ email: req.body.email });
+        // console.log(`<auth.js: ${isEmailExist.password}>`);
+
+        if (!isEmailExist) {
+            return res.status(400).json({ errors: `incrroct login cradintials.` });
+        }
+
+        //password match check
+        const isPasswordMatched = await bcrypt.compare(req.body.password, isEmailExist.password);
+
+        if (!isPasswordMatched) {
+            return res.status(400).json({ errors: `incrroct login cradintials.` });
+        }
+
+        //if email and password is correct then return the auth token
+        const data = {
+            user: {
+                id: isEmailExist.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({ authToken })
+
+
+    },
+);
+
 
 module.exports = router
