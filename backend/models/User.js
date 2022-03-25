d659772cf6a6f34724e0f61c39adfe5a5b392d21
Farhad <farhadmolla618@gmail.com>
2022-03-25 07:11:50 +0530

    video-47 one email auth

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,8 +4,7 @@ const {Schema} = mongoose;
 const UserSchema = new Schema({
     name:{
         type: String,
-        required: true,
-        unique: true
+        required: true
     },
     email:{
         type: String,
@@ -24,5 +23,5 @@ const UserSchema = new Schema({
 });
 
 const User = mongoose.model('user', UserSchema);
-User.createIndexes();
+// User.createIndexes();
 module.exports = User;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,25 +3,37 @@ const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 
-router.post('/',
-    [body('name').isLength({ min: 3 }),
-    body('email', '<email err>').isEmail(),
-    body('password').isLength({ min: 3 })],
-    (req, res) => {
+router.post(
+    '/creatuser',
+    [
+        body('name').isLength({ min: 3 }),
+        body('email', '<email err>').isEmail(),
+        body('password').isLength({ min: 3 })
+    ],
+    async (req, res) => {
         // Finds the validation errors in this request and wraps them in an object with handy functions
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
 
+        // check if the email already used
+        let isEmailUsed = await User.findOne({email: req.body.email});
+        // console.log(`<auth.js: ${isEmailUsed}>`);
+
+        if(isEmailUsed){
+            return res.status(400).json({ errors: `Use different email.` });
+        }
+
         User.create({
             name: req.body.name,
             email: req.body.email,
             password: req.body.password,
-        }).then(user => res.json(user))
-        .catch(err => {
-            res.json({error: err});
-        });
+        })
+            .then(user => res.json(user))
+            .catch(err => {
+                res.json({ error: err });
+            });
     },
 );
 
