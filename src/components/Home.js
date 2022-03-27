f30ba7db44de82ce0df3555f363c01187bd21537
Farhad <farhadmolla618@gmail.com>
2022-03-27 10:54:51 +0530

    video-59 form added in Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Form, Button, Container } from 'react-bootstrap'
 
 const Home = () => {
   return (
-    <div>
-      home
-    </div>
+    <>
+      <Container>
+        <Form>
+          <Form.Group className="mb-3" controlId="sdfsgxdsfg">
+            <Form.Label>Title</Form.Label>
+            <Form.Control type="text" placeholder="Enter a title" />
+            <Form.Text className="text-muted">
+              Title should be at list 3 character.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="sdfsdwws">
+            <Form.Label>Description</Form.Label>
+            <Form.Control as="textarea" aria-label="Give the description here" />
+            <Form.Text className="text-muted">
+              Description should be at list 3 character.
+            </Form.Text>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+
+    </>
   );
 }
 
